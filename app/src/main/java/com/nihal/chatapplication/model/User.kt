d8357cbb748b5d9b86ca1734535f3bd897315dcf
nihal <nixhalsam@gmail.com>
2021-02-09 13:39:38 +0100

    Added user model

---
+++ app/src/main/java/com/nihal/chatapplication/model/User.kt
@@ -0,0 +1,8 @@
+package com.nihal.chatapplication.model
+
+data class User(
+    var id: String = "",
+    var username: String = "",
+    val imageURL: String = "",
+    val searchName: String = ""
+)
