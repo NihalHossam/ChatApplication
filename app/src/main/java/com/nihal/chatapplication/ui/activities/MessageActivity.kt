91679f602a042b58df6afe79c1028bb179de9f59
nihal <nixhalsam@gmail.com>
2021-02-09 21:42:26 +0100

    Added sending messages method in message activity

--- app/src/main/java/com/nihal/chatapplication/ui/activities/MessageActivity.kt
+++ app/src/main/java/com/nihal/chatapplication/ui/activities/MessageActivity.kt
@@ -3,22 +3,33 @@ package com.nihal.chatapplication.ui.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nihal.chatapplication.BR
 import com.nihal.chatapplication.R
+import com.nihal.chatapplication.adapters.MessagesAdapter
 import com.nihal.chatapplication.databinding.ActivityMessageBinding
 import com.nihal.chatapplication.model.User
-import com.nihal.chatapplication.BR
+import com.nihal.chatapplication.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MessageActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMessageBinding
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var adapter: MessagesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_message)
         setContentView(binding.root)
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        initRecyclerView()
         getReceiverUser()
         setToolbar()
+        setSendMessageListener()
+        displayAllMessages()
     }
 
     private fun setToolbar() {
@@ -31,8 +42,33 @@ class MessageActivity : AppCompatActivity() {
         }
     }
 
+    private fun initRecyclerView() {
+        adapter = MessagesAdapter()
+        binding.chatMessagesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.chatMessagesRecyclerView.adapter = adapter
+    }
+
     private fun getReceiverUser() {
         val receiverUser: User = intent.getSerializableExtra("user") as User
+        userViewModel.receiver = receiverUser
         binding.setVariable(BR.user, receiverUser)
+
+    }
+
+    private fun setSendMessageListener() {
+        binding.sendButton.setOnClickListener {
+            val messageToSend = binding.textToSendEditText.text.toString()
+            if (messageToSend != "") {
+                userViewModel.sendMessage(messageToSend)
+            }
+            binding.textToSendEditText.setText("")
+        }
+    }
+
+    private fun displayAllMessages(){
+        userViewModel.getAllMessagesInChat().observe(this, { chatList ->
+            adapter.submitList(chatList)
+            adapter.notifyDataSetChanged()
+        })
     }
 }
