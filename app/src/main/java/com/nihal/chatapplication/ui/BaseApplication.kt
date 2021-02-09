913dd31b292b6dea0b80bf05c474f153116d8058
nihal <nixhalsam@gmail.com>
2021-02-09 13:35:16 +0100

    Added base application for hilt

---
+++ app/src/main/java/com/nihal/chatapplication/ui/BaseApplication.kt
@@ -0,0 +1,16 @@
+package com.nihal.chatapplication.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *  Triggers Hilt's code generation.
+ */
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
