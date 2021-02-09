bf4c1d7e89d69d2cb899fa8114a73e374ee8d9c0
nihal <nixhalsam@gmail.com>
2021-02-09 17:05:08 +0100

    Added user to be serializable

--- app/src/main/java/com/nihal/chatapplication/model/User.kt
+++ app/src/main/java/com/nihal/chatapplication/model/User.kt
@@ -1,8 +1,10 @@
 package com.nihal.chatapplication.model
 
+import java.io.Serializable
+
 data class User(
     var id: String = "",
     var username: String = "",
     val imageURL: String = "",
     val searchName: String = ""
-)
+): Serializable
