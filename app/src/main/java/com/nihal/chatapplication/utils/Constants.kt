07d63449905308bbbcd8c4928f88dad5c017dd33
nihal <nixhalsam@gmail.com>
2021-02-09 17:06:58 +0100

    Added app constants

---
+++ app/src/main/java/com/nihal/chatapplication/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.nihal.chatapplication.utils
+
+import android.view.View
+
+/**
+ * Constants used in the app.
+ */
+object Constants {
+    const val numberOfFragments = 3
+    const val VISIBLE = View.VISIBLE
+    const val GONE = View.GONE
+}
